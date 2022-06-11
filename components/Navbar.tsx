4a5f0d1cf0c1b01313dde29c15c3a1e7c620a776
daniebeler <hiebeler.daniel@gmail.com>
2022-06-12 00:57:08 +0200

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return (
+        <header className="border-b font-bold p-3 flex justify-center">
+            Algorithms
+        </header>
+    )
+}
+
+export default Navbar
